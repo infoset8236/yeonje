a0c9fb9356d932a0c6e6cba2e531a50b2a6bf677
infosetGang <kangyoonkyung@infoset.co.kr>
2025-05-21 11:12:55 +0900

    [feat] 디지털 갤러리 메인 작업중

---
+++ resources/ict/js/digitalGallery/digitalGalleryCommon.js
@@ -0,0 +1,58 @@
+// 슬릭 오류 방지
+jQuery.event.special.touchstart = {
+	setup: function (_, ns, handle) {
+		this.addEventListener('touchstart', handle, { passive: false });
+	},
+};
+jQuery.event.special.touchmove = {
+	setup: function (_, ns, handle) {
+		this.addEventListener('touchmove', handle, { passive: false });
+	},
+};
+
+$(document).ready(function () {
+	// 한손 모드 클릭 이벤트
+	$('.one_handed_mode').click(function () {
+		const content = $('.content');
+		const scroll = $('.scroll');
+		const image = $(this).find('img');
+		const targetTop = $(window).width() > 1080 ? 1920 : 960;
+		const scrollHeight = $(window).width() > 1080 ? 1560 : 780;
+
+		const isAtTargetTop = parseInt(content.css('top')) === targetTop;
+		content.css({ top: isAtTargetTop ? '0px' : targetTop + 'px' });
+
+		if (!isAtTargetTop) {
+			scroll.css({
+				'overflow-y': 'scroll',
+				height: scrollHeight + 'px',
+			});
+		} else {
+			scroll.css({
+				'overflow-y': '',
+				height: '100%',
+			});
+		}
+
+
+		if(scrollHeight === 1560 || 780) {
+			$('.full_mode').css('display', 'flex')
+		} else {
+			$('.full_mode').css('display', 'none')
+		}
+	});
+
+	// 전체 모드 클릭 이벤트
+	$('.full_mode').click(function () {
+		const content = $('.content');
+		const scroll = $('.scroll');
+		$('.full_mode').css('display', 'none')
+
+		content.css({ top: '0px' });
+		scroll.css({
+			'overflow-y': '',
+			height: '100%',
+		});
+		$('.full_mode').css('display', 'none');
+	});
+});
